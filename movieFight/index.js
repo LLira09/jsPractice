559dbfc042c32c61da7b1c7bce50841082f4817e
Larry Lira <larrylira@larrys-mbp.attlocal.net>
2020-02-10 16:50:13 -0600

    first commit

---
+++ movieFight/index.js
@@ -0,0 +1,23 @@
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'f1a9efb',
+      s: searchTerm
+    }
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = event => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+
+input.addEventListener('input', onInput);
